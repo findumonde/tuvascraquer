bdca9fe422351509f9e7b4e439957e068f7b86bc
Antoine Rousseau <antoine@rousseau.im>
2020-04-08 14:47:29 +0200

    test

--- src/gatsby/onRenderBody.tsx
+++ src/gatsby/onRenderBody.tsx
@@ -1,13 +1,24 @@
 import React from "react"
 
 const onRenderBody: GatsbySsr.OnRenderBody = ({ setHeadComponents }) => {
-  setHeadComponents([
+  const tags = [
     <link
       key="fonts"
       href="https://fonts.googleapis.com/css2?family=Boogaloo&family=Open+Sans:ital,wght@0,400;1,300" // &display=swap
       rel="stylesheet"
     />,
-  ])
+  ]
+  if (process.env.GATSBY_PUB) {
+    tags.push(
+      <script
+        key="ads"
+        data-ad-client={process.env.GATSBY_PUB}
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+      />
+    )
+  }
+  setHeadComponents(tags)
 }
 
 export default onRenderBody
