56e555b8ec80c98f630d5803972127eb514d6d1d
Antoine Rousseau <antoine@rousseau.im>
2020-04-11 14:51:17 +0200

    url params

--- src/components/LangContext.tsx
+++ src/components/LangContext.tsx
@@ -2,7 +2,7 @@ import React, { useContext, useState, useEffect } from "react"
 
 import { Localized } from "src/types"
 
-import { isBrowser } from "src/helpers/window"
+import { isBrowser, getUrlParam } from "src/helpers/window"
 import { DEFAULT_COUNTRY } from "src/helpers/constants"
 import TRANSLATIONS from "src/translations"
 
@@ -21,9 +21,10 @@ interface Data {
 
 export const localize = (data: Data) => {
   if (isBrowser() && data.localized) {
+    const lang = getUrlParam("lang") || navigator.language
     for (const locales in data.localized) {
       const list = locales.split(",")
-      if (list.includes(navigator.language)) {
+      if (list.includes(lang)) {
         return data.localized[locales]
       }
     }
@@ -35,15 +36,20 @@ export const LangProvider: React.FC = ({ children }) => {
   const [country, setCountry] = useState<string>()
 
   useEffect(() => {
-    fetch(`/cdn-cgi/trace`)
-      .then((response) => response.text())
-      .then((data) => {
-        const match = data.match(/loc=([A-Z]+)$/m)
-        setCountry((match && match[1]) || DEFAULT_COUNTRY)
-      })
-      .catch(() => {
-        setCountry(DEFAULT_COUNTRY)
-      })
+    const code = getUrlParam("country")
+    if (code) {
+      setCountry(code)
+    } else {
+      fetch(`/cdn-cgi/trace`)
+        .then((response) => response.text())
+        .then((data) => {
+          const match = data.match(/loc=([A-Z]+)$/m)
+          setCountry((match && match[1]) || DEFAULT_COUNTRY)
+        })
+        .catch(() => {
+          setCountry(DEFAULT_COUNTRY)
+        })
+    }
   }, [])
 
   return <LangContext.Provider value={{ country }}>{children}</LangContext.Provider>
